6a34a24078c22c3c07e40ed5c37fcc92d1cb95fc
mrsalvini <dsgmcosta@gmail.com>
2019-06-26 23:25:13 +0100

    login and register handled in front end

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { login } from '../../actions/auth'
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -13,8 +16,10 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault()
-    console.log('success')
+    login(email, password)
   }
+
+  if (isAuthenticated) return <Redirect to='/dashboard' />
 
   return (
     <Fragment>
@@ -40,7 +45,7 @@ const Login = () => {
             required
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Register" />
+        <input type="submit" className="btn btn-primary" value="Login" />
       </form>
       <p className="my-1">
         Don't have an account? <Link to='/register'>Sign Up</Link>
@@ -49,4 +54,13 @@ const Login = () => {
   )
 }
 
-export default Login
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { login })(Login)
