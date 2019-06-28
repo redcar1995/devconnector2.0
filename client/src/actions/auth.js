9a65b0fa48d554695e6a7d4731399952cebc70d7
mrsalvini <dsgmcosta@gmail.com>
2019-06-28 22:06:42 +0100

    doing forms

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,9 @@ import {
   USER_LOADED,
   AUTH_ERROR,
   LOGIN_SUCCESS,
-  LOGIN_FAIL
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_PROFILE
 } from './types'
 import setAuthToken from '../utils/setAuthToken';
 
@@ -90,4 +92,10 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     })
   }
+}
+
+// Logout / Clear profile
+export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE })
+  dispatch({ type: LOGOUT })
 }
