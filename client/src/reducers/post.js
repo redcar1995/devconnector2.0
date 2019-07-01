4efbf3a7da667af867650004ed330e1b4d7ce94b
mrsalvini <dsgmcosta@gmail.com>
2019-07-01 11:37:19 +0100

    profiles complete

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,16 +1,13 @@
 import {
-  GET_PROFILE,
-  GET_PROFILES,
-  GET_REPOS,
-  PROFILE_ERROR,
-  CLEAR_PROFILE,
-  UPDATE_PROFILE
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST
 } from '../actions/types'
 
 const initialState = {
-  profile: null,
-  profiles: [],
-  repos: [],
+  posts: [],
+  post: null,
   loading: true,
   error: {}
 }
@@ -19,36 +16,28 @@ export default function (state = initialState, action) {
   const { type, payload } = action
 
   switch (type) {
-    case GET_PROFILE:
-    case UPDATE_PROFILE:
+    case GET_POSTS:
       return {
         ...state,
-        profile: payload,
+        posts: payload,
         loading: false
       }
-    case GET_PROFILES:
+    case DELETE_POST:
       return {
         ...state,
-        profiles: payload,
+        posts: state.posts.filter(post => post._id !== payload),
         loading: false
       }
-    case PROFILE_ERROR:
+    case POST_ERROR:
       return {
         ...state,
         error: payload,
         loading: false
       }
-    case CLEAR_PROFILE:
+    case UPDATE_LIKES:
       return {
         ...state,
-        profile: null,
-        repos: [],
-        loading: false
-      }
-    case GET_REPOS:
-      return {
-        ...state,
-        repos: payload,
+        posts: state.posts.map(post => post._id === payload.postId ? { ...post, likes: payload.likes } : post),
         loading: false
       }
     default:
